107d3ecf7327e6f0fc0d2274f272cb1efb14c115
Yusuf Wisnu <yusufnagashi@ymail.com>
2019-12-21 21:39:46 +0700

    a

--- magang-warehouse/rest-api/MainApps/urls.py
+++ magang-warehouse/rest-api/MainApps/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from Items.views import ItemViewSet
+from Transactions.views import OutComingViewSet, InComingViewSet
+from Users.views import UserViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('item', ItemViewSet)
+router.register('out_transactions', OutComingViewSet)
+router.register('in_transactions', InComingViewSet)
+router.register('users', UserViewSet)
 
 
 urlpatterns = [
--- magang-warehouse/rest-api/Users/views.py
+++ magang-warehouse/rest-api/Users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CustomUserModel
+from .serializers  import UserSerializers
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUserModel.objects.all()
+    serializer_class = UserSerializers
